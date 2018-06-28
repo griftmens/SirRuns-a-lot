169b2b302003e5f6c397885c6700638f9a687a29
Joaquin Olazabal <joaquin_oa@hotmail.com>
2018-06-28 15:21:36 -0500

    Arreglo de scripts y orden

---
+++ Limpiuo/Assets/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public float v = 1f;
+
+	// Use this for initialization
+	void Start () {
+		//GetComponent<Rigidbody>().velocity = Vector3.down * v;
+		
+		Physics.gravity *= 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+		GetComponent<Rigidbody>().AddForce (Vector3.up * v, ForceMode.Impulse);
+		
+	}
+}
--- Limpiuo/Assets/Scripts/Managers/PlayerManager.cs
+++ Limpiuo/Assets/Scripts/Managers/PlayerManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof (Rigidbody))]
 public class PlayerManager : MonoBehaviour {
 
 	public static Transform playerRef;
@@ -24,19 +25,22 @@ public class PlayerManager : MonoBehaviour {
 	}
 	
 	void Update () {
-		if (moving) {
-			t += Time.deltaTime;
-			Vector3.Lerp(startPos, targetPos, t * transitionSpeed);
-			if (Vector3.Distance(startPos,targetPos) <= 0.1f){
-				moving = false;
-				t = 0;
-			}
-		}
+		// if (moving) {
+		// 	t += Time.deltaTime;
+		// 	Vector3.Lerp(startPos, targetPos, t * transitionSpeed);
+		// 	if (Vector3.Distance(startPos,targetPos) <= 0.1f){
+		// 		moving = false;
+		// 		t = 0;
+		// 	}
+		// }
+
+		if (Input.GetKeyDown(KeyCode.Space))
+			Receptor("Up");
 	}
 
 	void SetPosition () {
 		startPos = transform.position;
-		targetPos = LevelManager.instance.lines[playerInfo.currentLine].position;
+		targetPos = startPos; //LevelManager.instance.lines[playerInfo.currentLine].position;
 		moving = true;
 	}
 
@@ -44,8 +48,9 @@ public class PlayerManager : MonoBehaviour {
 
 		switch (command) {
 			case "Up":
-				rb.AddForce (Vector3.up * playerInfo.jumpForce, ForceMode.Impulse);
+				rb.AddForce (Vector3.up * playerInfo.jumpForce, ForceMode.VelocityChange);
 				//Anim
+				rb.velocity = Vector3.down * 3;
 			break;
 			case "Down":
 				//Slide Anim
