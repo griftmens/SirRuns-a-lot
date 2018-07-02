794351b4e5cca35a2b18a08a18ff734a685adb19
Joaquin Olazabal <joaquin_oa@hotmail.com>
2018-07-02 08:32:36 -0500

    Menu de pausa y regreso a pantalla principal

---
+++ Assets/Scripts/MainScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainScene : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        Debug.Log("entra");
+        SceneManager.LoadScene("00-Test");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Sale");
+        SceneManager.LoadScene("MainMenu");
+    }
+}
--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -7,16 +7,54 @@ using UnityEngine.UI;
 
 public class Pause : MonoBehaviour {
 
-	public 
+    bool pause = false;
+    public Image PMenu;
+    public Image Resume;
+    public Image Exit;
+
+    // Use this for initialization
+    void Start () {
+
+        PMenu.enabled = false;
+        Resume.enabled = false;
+        Exit.enabled = false;
+        Time.timeScale = 1;
 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void Pausa()
+    {
+        if (pause == false)
+        {
+            PMenu.enabled = true;
+            Resume.enabled = true;
+            Exit.enabled = true;
+            Time.timeScale = 0;
+            pause = true;
+        }
+        else
+        {
+            PMenu.enabled = false;
+            Resume.enabled = false;
+            Exit.enabled = false;
+            Time.timeScale = 1;
+            pause = false;
+        }
+
+    }
+
+    public void Play()
+    {
+        PMenu.enabled = false;
+        Resume.enabled = false;
+        Exit.enabled = false;
+        Time.timeScale = 1;
+        pause = false;
+    }
 }
